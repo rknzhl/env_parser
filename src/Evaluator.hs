ddee9d571288fa6cbc423376fed30c3917195235
den1shh <dengud79@gmail.com>
2026-05-15 23:00:43 +0300

    Evaluator: buildDefs и проверка дубликатов

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -14,8 +14,24 @@ prettyEvalError (UnknownVariable n) = "unknown variable: $" ++ n
 prettyEvalError (CycleDetected ns) = "cycle detected: " ++ joinWith " -> " ns
 prettyEvalError (DuplicateVariable n) = "duplicate variable: " ++ n
 
-evalEnv :: EnvFile -> Either EvalError [(Name, String)]
-evalEnv = error "TODO: завтра"
+type Defs = [(Name, Value)]
+type Env = [(Name, String)]
+
+evalEnv :: EnvFile -> Either EvalError Env
+evalEnv (EnvFile bs) =
+  case buildDefs bs of
+    Left err -> Left err
+    Right _ -> Right []
+
+buildDefs :: [Binding] -> Either EvalError Defs
+buildDefs [] = Right []
+buildDefs (Binding name val : rest) =
+  case buildDefs rest of
+    Left err -> Left err
+    Right defs ->
+      case lookup name defs of
+        Just _ -> Left (DuplicateVariable name)
+        Nothing -> Right ((name, val) : defs)
 
 joinWith :: String -> [String] -> String
 joinWith _ [] = ""
