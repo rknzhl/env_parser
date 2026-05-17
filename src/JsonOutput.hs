ea04a705ab62fcffe6bc37a6ce413421fd3443fb
Ibragimova Kseniia <ibragimova.kr@phystech.edu>
2026-05-17 12:06:51 +0300

    JsonOutput  ковычки

--- src/JsonOutput.hs
+++ src/JsonOutput.hs
@@ -4,7 +4,18 @@ encodeEnv :: [(String, String)] -> String
 encodeEnv pairs = "{" ++ joinWith ", " (map encodePair pairs) ++ "}"
 
 encodePair :: (String, String) -> String
-encodePair (k, v) = "\"" ++ k ++ "\": \"" ++ v ++ "\""
+encodePair (k, v) = "\"" ++ escapeJson k ++ "\": \"" ++ escapeJson v ++ "\""
+
+escapeJson :: String -> String
+escapeJson [] = []
+escapeJson ('"' : xs) = "\\\"" ++ escapeJson xs
+escapeJson ('\\' : xs) = "\\\\" ++ escapeJson xs
+escapeJson ('\n' : xs) = "\\n" ++ escapeJson xs
+escapeJson ('\t' : xs) = "\\t" ++ escapeJson xs
+escapeJson ('\r' : xs) = "\\r" ++ escapeJson xs
+escapeJson ('\b' : xs) = "\\b" ++ escapeJson xs
+escapeJson ('\f' : xs) = "\\f" ++ escapeJson xs
+escapeJson (x : xs) = x : escapeJson xs
 
 joinWith :: String -> [String] -> String
 joinWith _ [] = ""
